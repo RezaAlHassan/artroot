09be9fcfe43f161a5a3ae702d160403d73ad9a01
RezaAlHassan <rezahasan1198@gmail.com>
2023-01-31 01:17:21 +0600

    messages fade after delay

--- resources/views/add-art.blade.php
+++ resources/views/add-art.blade.php
@@ -16,7 +16,7 @@
 
 <body>
   @if(session('status'))
-  <p class="success-message center text-center">{{session('status')}}</p>
+  <p id="header-message" class="success-message center text-center">{{session('status')}}</p>
   @endif
   <nav class="navbar navbar-expand-lg ">
         <div class="container-fluid">
@@ -115,5 +115,13 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script > 
+    setTimeout(() => {
+    const box = document.getElementById('header-message');
+  
+    box.style.display = 'none';
+  
+    }, 2500);
+    </script>
 
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,7 +16,7 @@
 
 <body>
   @if(session('status'))
-  <p class="info-message center text-center">{{session('status')}}</p>
+  <p id="header-message" class="info-message center text-center">{{session('status')}}</p>
   @endif
     <nav class="navbar sticky-top navbar-expand-lg ">
         <div class="container-fluid">
@@ -171,5 +171,14 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script > 
+  setTimeout(() => {
+  const box = document.getElementById('header-message');
+
+  // 👇️ removes element from DOM
+  box.style.display = 'none';
+
+}, 2500); // 👈️ time in milliseconds
+  </script>
 
 </html>
